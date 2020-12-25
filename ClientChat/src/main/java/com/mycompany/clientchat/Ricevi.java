d24a829476c2fbe8c7a6103c4309d546ad051c51
minatore06 <35301775+minatore06@users.noreply.github.com>
2020-12-25 13:03:34 +0100

    notification, upload

--- ClientChat/src/main/java/com/mycompany/clientchat/Ricevi.java
+++ ClientChat/src/main/java/com/mycompany/clientchat/Ricevi.java
@@ -2,6 +2,9 @@ package com.mycompany.clientchat;
 
 import java.io.*;
 import java.net.Socket;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 public class Ricevi extends Thread{
     BufferedReader inputServer;
     Socket socket;
@@ -12,6 +15,7 @@ public class Ricevi extends Thread{
         for(;;){//ripetizione infinita
             try {
                 stringa = inputServer.readLine();//ricezione messaggio
+                playSound("/kiniro-mosaic-ayaya-ayaya.wav");
                 //controllo se è stata richiesta una disconnessione dal server
                 if(stringa.equals("Logout")){
                     gui.addMsg("Disconnesso\r\n");//aggiunta a schermo del messaggio di disconnessione nella gui
@@ -28,6 +32,23 @@ public class Ricevi extends Thread{
         }
     }
     
+    public static synchronized void playSound(final String url) {
+        new Thread(new Runnable() {
+        // The wrapper thread is unnecessary, unless it blocks on the
+        // Clip finishing; see comments.
+          public void run() {
+            try {
+              Clip clip = AudioSystem.getClip();
+              AudioInputStream inputStream = AudioSystem.getAudioInputStream(this.getClass().getResourceAsStream("/sounds" + url));
+              clip.open(inputStream);
+              clip.start(); 
+            } catch (Exception e) {
+              System.err.println(e.getMessage());
+            }
+          }
+        }).start();
+      }
+
     public Ricevi(BufferedReader input, ClientChat gui){
         inputServer= input;
         this.gui = gui;
